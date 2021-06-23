b3b4db0bd948d6691e373190ba5fe93606f7c38b
sachuckm <sachu.ckm@gmail.com>
2021-06-23 19:30:20 +0530

    Delete src directory

--- src/Reducers/reducer_root.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import reduceReducers from "reduce-reducers";
-
-export default combineReducers({
-  // user data is preloaded only for now, so it should have a no op reducer
-  user: (state) => state || null,
-  common: combineReducers({}),
-});
